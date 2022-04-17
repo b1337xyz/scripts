be1e054854c0d941c1d200104b0e6e77c1daad74
A1337Xyz <blindwizard@tutanota.com>
2022-04-17 12:43:41 -0300

    added more sources for radio.sh

---
+++ shell/radio/radio.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+command -v dmenu >/dev/null 2>&1 || { echo 'install dmenu'; exit 1; }
+command -v mpv >/dev/null 2>&1 || { echo 'install mpv'; exit 1; }
+
+set -eo pipefail
+
+src=$(dirname "$(realpath "$0")")
+conf="${src}/radio.txt"
+url=$(
+    cut -d' ' -f1 "$conf"   |
+    dmenu -l 10 -i -c       |
+    xargs -rI{} awk -F'|' '/^{}/{print $2}' "$conf"
+)
+[ -z "$url" ] && exit 1
+xterm -name floating_terminal   \
+    -title radio                \
+    -e "mpv --profile=radio \"$url\"" >/dev/null 2>&1
