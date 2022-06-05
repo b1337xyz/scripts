15937c46e7fc352ba8d91fb7e65377b336749a0e
A1337Xyz <blindwizard@tutanota.com>
2022-06-05 20:34:21 -0300

    using bs4 to parse html

--- python/nhentai.py
+++ python/nhentai.py
@@ -4,6 +4,7 @@ import re
 import json
 import requests
 import subprocess as sp
+from bs4 import BeautifulSoup as BS
 from optparse import OptionParser
 
 # MAKE SURE YOU USE THE SAME IP AND USERAGENT AS WHEN YOU GOT YOUR COOKIE!
@@ -17,6 +18,10 @@ parser.add_option(
 parser.add_option(
     '-c', '--cookie', dest='cookie', metavar="STR",
     help="csrftoken=TOKEN; sessionid=ID; cf_clearance=CLOUDFLARE"
+)
+parser.add_option(
+    '-i', '--input-file', dest='input_file', metavar='FILE',
+    action='store', help='Download URLs found in FILE'
 )
 parser.add_option('-u', '--user-agent', dest='user_agent', metavar="STR")
 opts, args = parser.parse_args()
@@ -43,46 +48,21 @@ if opts.cookie or opts.user_agent:
     with open(CONFIG, 'w') as fp:
         json.dump(config, fp)
 
-if len(args) == 0:
+if len(args) == 0 and not opts.input_file:
     parser.error('<url> not provided')
 
-URL = args[0]
-if not re.match(r'^https://nhentai', URL):
-    parser.error('Invalid URL')
-if 'page=' in URL:
-    URL = re.sub(r'([\?&]page=)\d*', r'\1{}', URL)
-elif '?' in URL:
-    URL += '&page={}'
-else:
-    URL += '?page={}'
 
-
-DL_DIR = os.path.realpath(opts.dl_dir)
-if not os.path.exists(DL_DIR):
-    os.mkdir(DL_DIR)
-assert os.path.isdir(DL_DIR), f'"{DL_DIR}" not a directory'
-
-try:
-    tag = URL.split('?')[0].split('/')[4]
-    if tag:
-        DL_DIR = os.path.join(DL_DIR, tag)
-        if not os.path.exists(DL_DIR):
-            os.mkdir(DL_DIR)
-except AttributeError:
-    pass
-
-
-def get_html(url):
+def get_soup(url):
     r = requests.get(
         url,
         cookies={'required_cookie': COOKIE},
         headers={'User-Agent': UA}
     )
-    return r.text
+    return BS(r.text, 'html.parser')
 
 
-def download(url, fname):
-    file = os.path.join(DL_DIR, fname)
+def download(url, dl_dir, fname):
+    file = os.path.join(dl_dir, fname)
     if not os.path.exists(file):
         try:
             r = requests.get(
@@ -94,45 +74,64 @@ def download(url, fname):
                 fp.write(r.content)
         except Exception as err:
             print(f'Failed to download torrent "{url}"\nError: {err}')
-    mime = sp.run(['file', '-bi', file], stdout=sp.PIPE).stdout.decode()
-    if not 'bittorrent' in mime:
-        os.remove(file)
-        return
-    out = sp.run(['aria2c', '-S', file], stdout=sp.PIPE).stdout.decode()
-    try:
-        torrent_name = re.search(r' 1\|\./([^/]*)', out).group(1)
-    except AttributeError:
-        os.remove(file)
-        return
-    torrent = os.path.join(DL_DIR, torrent_name)
-    if os.path.exists(torrent):
-        os.remove(file)
-        return
-    if not 'english' in torrent_name.lower():
-        os.remove(file)
-        return
 
     if sp.run([
-        'aria2c', '--dir', DL_DIR,
+        'aria2c', '--dir', dl_dir,
         '--log-level=error', '--console-log-level=error',
         '--bt-stop-timeout=500', '--seed-time=0', file
     ]).returncode == 0:
         os.remove(file)
 
 
-def main():
+def main(URL):
+    if 'page=' in URL:
+        URL = re.sub(r'([\?&]page=)\d*', r'\1{}', URL)
+    elif '?' in URL:
+        URL += '&page={}'
+    else:
+        URL += '?page={}'
+
+    dl_dir = os.path.realpath(opts.dl_dir)
+    if not os.path.exists(dl_dir):
+        os.mkdir(dl_dir)
+    assert os.path.isdir(dl_dir), f'"{dl_dir}" not a directory'
+
+    try:
+        tag = URL.split('?')[0].split('/')[4]
+        if tag:
+            dl_dir = os.path.join(dl_dir, tag)
+            if not os.path.exists(dl_dir):
+                os.mkdir(dl_dir)
+    except AttributeError:
+        pass
+
     page = 1
+    posts = list()
     while True:
-        url = URL.format(page)
-        html = get_html(url)
-        posts = re.findall(r'href="([^""]*/g/\d*)', html)
-        if not posts:
+        soup = get_soup(URL.format(page))
+        gallery = soup.findAll('div', {'class': 'gallery'})
+        if not gallery:
             break
-        for i in posts:
-            url = f'https://nhentai.net{i}/download'
-            fname = i.split('/')[-1] + '.torrent'
-            download(url, fname)
+        for div in gallery:
+            a = div.a.get('href')
+            title = div.text.strip()
+            if not 'english' in title.lower():
+                continue
+            posts.append(a)
+        page += 1
+
+    for i, post in enumerate(posts, start=1):
+        url = f'https://nhentai.net{post}/download'
+        print(f'[{i}/{len(posts)}] {url}')
+        fname = post.split('/')[-1] + '.torrent'
+        download(url, dl_dir, fname)
 
 
 if __name__ == '__main__':
-    main()
+    if opts.input_file:
+        with open(opts.input_file, 'r') as fp:
+            args = [i.strip() for i in fp.readlines() if 'nhentai.net' in i]
+    for i in args:
+        if not re.match(r'^https://nhentai', i):
+            parser.error('Invalid URL')
+        main(i)
