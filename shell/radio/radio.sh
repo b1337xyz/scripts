7313f70838503ee381cb324bde46ef1cd22c6e6a
A1337Xyz <blindwizard@tutanota.com>
2022-06-11 23:33:54 -0300

    update radio.txt

--- shell/radio/radio.sh
+++ shell/radio/radio.sh
@@ -7,15 +7,20 @@ command -v mpv >/dev/null 2>&1 ||
 
 set -eo pipefail
 
+FIFO=/tmp/mpvradio
 src=$(dirname "$(realpath "$0")")
 conf="${src}/radio.txt"
 url=$(
-    cut -d' ' -f1 "$conf"   |
-    dmenu -l 10 -i -c       |
+    cut -d'|' -f1 "$conf"   |
+    dmenu -l 20 -i -c       |
     xargs -rI{} grep -F "{}" "$conf" |
     cut -d'|' -f2
 )
 [ -z "$url" ] && exit 1
-xterm -name floating_terminal   \
-    -title radio                \
-    -e "mpv --profile=radio \"$url\"" >/dev/null 2>&1
+if [ -S "$FIFO" ];then
+    echo '{"command": ["loadfile", "'"$url"'"]}' | socat - "$FIFO"
+else
+    alacritty --class floating_terminal     \
+        --title radio                       \
+        -e mpv --profile=radio "$url" #>/dev/null 2>&1
+fi
