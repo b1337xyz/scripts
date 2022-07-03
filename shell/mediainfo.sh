eb8979c20a620e184c8de07621f9f927e7015eec
A1337Xyz <blindwizard@tutanota.com>
2022-07-02 21:11:23 -0300

    updated mediainfo functions

--- shell/mediainfo.sh
+++ shell/mediainfo.sh
@@ -1,85 +1,75 @@
 #!/usr/bin/env bash
-convert_mms() {
-    local hh ss mm
-    hh=0
-    ss=$(( $1 / 1000 ))
-    mm=$(( ss / 60 ))
-    ss=$(( ss - ( mm * 60 ) ))
-    if [ "$mm" -ge 60 ];then
-        hh=$(( mm / 60 ))
-        mm=$(( mm % 60 ))
-    fi
-    [ "$hh" -lt 10 ] && hh="0$hh"
-    [ "$mm" -lt 10 ] && mm="0$mm"
-    [ "$ss" -lt 10 ] && ss="0$ss"
-    printf '%s:%s:%s' "$hh" "$mm" "$ss"
-}
 lsd() {
-    local total=0
-    if [ -f "$1" ];then
-        for i in "$@";do
-            mms=$(mediainfo --Output='General;%Duration%' "$i" | cut -d'.' -f1)
-            [ -z "$mms" ] && return 1
-            (( total += mms ))
-            duration=$(convert_mms "$mms") 
-            printf '\e[1;34m%s\e[m - \e[1;35m%s\e[m\n' "$duration" "$i"
-        done
-    else
-        while read -r i;do
-            # mediainfo --Output='General;%CompleteName%: %Duration/String%' "$i";
-            mms=$(mediainfo --Output='General;%Duration%' "$i")
-            [ -z "$mms" ] && continue
-            (( total += mms ))
-            duration=$(convert_mms "$mms") 
-            printf '\e[1;34m%s\e[m - \e[1;35m%s\e[m\n' "$duration" "$i"
-        done < <(find "${1:-.}" -mindepth 1 -maxdepth 1 \
-            -iregex '.*\.\(mp[3-4]\|wav\|opus\|mkv\|avi\|webm\)' | sort)
-    fi
+    find "${@:-.}" -maxdepth 1 -iregex '.*\.\(mp3\|opus\|mkv\|mp4\|m4v\|mov\|webm\|avi\)' -print0 |
+    sort -zV | xargs -r0 mediainfo --Output=JSON | jq -Mcr '
+.[].media | select(.track[]["@type"] == "Video") | 
+[
+    .["@ref"],
+    ( .track[] | select(.["@type"] == "General") | .Duration )
+] | "\(.[0]);\(.[1])"' | awk -F';' 'BEGIN { total = 0 }
 
-    [ "$total" -eq 0 ] && return 1
-    if [ "$total" -ne "$mms" ];then
-        duration=$(convert_mms "$total")
-        printf 'Total: \e[1;32m%s\e[m\n' "$duration"
-    fi
+function convert_mms(n) {
+    ss = int( (n + 0) / 1000)
+    hh = int(ss / 3600)
+    mm = int(ss / 60) % 60
+    if (ss >= 60) ss %= 60
+    if ( mm >= 60 ) mm %= 60
+    if ( hh < 10 ) hh = "0"hh
+    if ( mm < 10 ) mm = "0"mm
+    if ( ss < 10 ) ss = "0"ss
+
+    return hh":"mm":"ss
+}
+
+{
+    sub(/\./, "", $2)
+    total += $2 + 0
+    d = convert_mms($2)
+    printf("\033[1;34m%s:\033[m - \033[1;35m%s\033[m\n", d, $1)
+
+} END { printf("Total: \033[1;32m%s\033[m\n", convert_mms(total)) }'
 }
 lsres() {
-    find "${@:-.}" -iregex '.*\.\(webp\|jpg\|png\|jpeg\|mp4\|avi\|webm\|gif\|m4v\|mkv\)' -print0 |
+    find "${@:-.}" -maxdepth 1 -iregex '.*\.\(webp\|jpg\|png\|jpeg\|mp4\|avi\|webm\|gif\|m4v\|mkv\)' -print0 |
         xargs -r0 mediainfo --Output=JSON |
         jq -Mcr '.[].media? // empty |
         [
             .["@ref"],
-            ( (.track | .. | .Width?  // empty) | tonumber ),
-            ( (.track | .. | .Height? // empty) | tonumber ) 
+            ((.track[] |
+                select(.["@type"] == "Image" or .["@type"] == "Video"
+            ) | .Width) | tonumber ),
+            ((.track[] |
+                select(.["@type"] == "Image" or .["@type"] == "Video"
+            ) | .Height) | tonumber )
         ] | "\(.[1])x\(.[2]) \(.[0])"'
     # printf '%sx%-4s %s\n' "$width" "$height" "$i"
 }
-all1080p() {
-    find . -maxdepth 1 -type f -iname '*.jpg' | while read -r i;do
-        out="1080p_${i##*/}"
-        IFS='x' read -r width height < <(mediainfo --Output='Image;%Width%x%Height%' "$i")
-        [ -z "$width" ] && continue
-        [ -z "$height" ] && continue
-        [ "$width" -le 1920 ] && continue
-        [ "$height" -le 1080 ] && continue
-        [ "$width" -lt "$height" ] && continue
-        convert -verbose -resize 1920x1080\! "$i" "$out" && rm -vf "$i"
-    done
-}
 killflac() {
-    find "${1:-.}" -type f -iregex '.*\.\(mkv\|mp4\)' | sort | while read -r i;do
-        mediainfo --Output='Audio;%Format%' "$i" |
-            grep -qi flac && printf '%s\n' "$i"
-    done
-
-    return 0
-}
-killflac2() {
-    find "${1:-.}" -iregex '.*\.\(mkv\|mp4\)' -print0 |
+    find "${@:-.}" -iregex '.*\.\(mkv\|mp4\)' -print0 |
     xargs -r0  mediainfo --Output=JSON |
     jq -Mrc '
     [
-        .media["@ref"]
-    ] + [
-        .media.track[] | select(.["@type"] == "Audio") | .Format
+        .media["@ref"],
+        (.media.track[] | select(.["@type"] == "Audio") | .Format)
     ] | select(.[1] == "FLAC") | .[0]' 2>/dev/null
+}
+tall() {
+    out=tall
+    [ -d "$out" ] || { mkdir -v "$out" || return 1; }
+    find . -maxdepth 1 -iregex '.*\.\(jpg\|png\|jpeg\|mp4\|webm\|gif\)' -print0 |
+    xargs -P 2 -r0 mediainfo --Output=JSON |
+    jq -Mcr '.[].media? |
+    [
+        .["@ref"],
+        ((.track[] |
+            select(.["@type"] == "Image" or .["@type"] == "Video"
+        ) | .Width) | tonumber ),
+        ((.track[] |
+            select(.["@type"] == "Image" or .["@type"] == "Video"
+        ) | .Height) | tonumber )
+    ] |
+    select(.[2] > .[1]) | .[0]' | while read -r i;
+    do
+        mv -vn -- "$i" "$out"
+    done
 }
