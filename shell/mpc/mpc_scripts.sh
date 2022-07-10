54dc260f3820630bf18f20908b79c3f730adb518
A1337Xyz <blindwizard@tutanota.com>
2022-07-10 16:28:12 -0300

    mpc script removed

--- shell/mpc/mpc_scripts.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-command -v dmenu &>/dev/null || { printf 'install dmenu\n'; exit 1; }
-scripts='/home/erik/.scripts/ShellScript/mpc_scripts'
-for i in "$scripts"/*.sh;do
-    [ "${i##*/}" = "${0##*/}" ] && continue
-    basename "$i"
-done | dmenu -l 15 -p 'mpc_scripts:' | xargs -rI{} bash "$scripts/{}"
