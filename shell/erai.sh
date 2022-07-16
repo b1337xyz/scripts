fbf2103352727270decb20ab23f27917a65fc76b
A1337Xyz <blindwizard@tutanota.com>
2022-07-15 22:26:49 -0300

    add function download_all

--- shell/erai.sh
+++ shell/erai.sh
@@ -1,48 +1,49 @@
 #!/usr/bin/env bash
-
-command -v fzf &>/dev/null || { printf 'install fzf\n'; exit 1; }
-
+declare -r -x DLDIR=~/Downloads
 declare -r -x domain="https://txt.erai-raws.org"
-declare -r -x tmpfile=$(mktemp)
 declare -r -x dlicon=folder-download
 declare -r -x favicon=emblem-favorite-symbolic.symbolic
 declare -r -x delicon=user-trash
-declare -r -x cache=~/.cache/erai.txt
+declare -r -x favorites=~/.cache/erai.txt
+declare -r -x tmpfile=$(mktemp)
 
 end() { rm "$tmpfile"; }
 trap end EXIT
 
 quote() {
     sed '
-    s/%/%25/g;  s/ /%20/g;  s/\[/%5B/g;
-    s/\]/%5D/g; s/</%3C/g;  s/>/%3E/g;
-    s/#/%23/g;  s/{/%7B/g;  s/}/%7D/g;
-    s/|/%7C/g;  s/\\/%5C/g; s/\^/%5E/g;
-    s/~/%7E/g;  s/`/%60/g;  s/\;/%3B/g;
-    s/?/%3F/g;  s/@/%40/g;  s/=/%3D/g;
-    s/&/%26/g;  s/\$/%24/g'
+    s/%/%25/g;    s/ /%20/g;    s/\[/%5B/g;
+    s/\]/%5D/g;   s/</%3C/g;    s/>/%3E/g;
+    s/#/%23/g;    s/{/%7B/g;    s/}/%7D/g;
+    s/|/%7C/g;    s/\\/%5C/g;   s/\^/%5E/g;
+    s/~/%7E/g;    s/`/%60/g;    s/\;/%3B/g;
+    s/?/%3F/g;    s/@/%40/g;    s/=/%3D/g;
+    s/&/%26/g;    s/\$/%24/g;   s/(/%28/g;
+    s/)/%29/g;    s/!/%21/g;    s/Δ/%CE%94/g
+    '
 }
 unquote() {
     sed '
-    s/%25/%/g;  s/%20/ /g;  s/%5B/\[/g;
-    s/%5D/\]/g; s/%3C/</g;  s/%3E/>/g;
-    s/%23/#/g;  s/%7B/{/g;  s/%7D/}/g;
-    s/%7C/|/g;  s/%5C/\\/g; s/%5E/\^/g;
-    s/%7E/~/g;  s/%60/`/g;  s/%3B/\;/g;
-    s/%3F/?/g;  s/%40/@/g;  s/%3D/=/g;
-    s/%26/&/g;  s/%24/\$/g'
+    s/%25/%/g;    s/%20/ /g;    s/%5B/\[/g;
+    s/%5D/\]/g;   s/%3C/</g;    s/%3E/>/g;
+    s/%23/#/g;    s/%7B/{/g;    s/%7D/}/g;
+    s/%7C/|/g;    s/%5C/\\/g;   s/%5E/\^/g;
+    s/%7E/~/g;    s/%60/`/g;    s/%3B/\;/g;
+    s/%3F/?/g;    s/%40/@/g;    s/%3D/=/g;
+    s/%26/&/g;    s/%24/\$/g;   s/%28/(/g;
+    s/%29/)/g;    s/%21/!/g;    s/%CE%94/Δ/g
+    '
 }
 main() {
+    url=
     if [[ "${1##*.}" =~ (ass|srt) ]];then
         dir=${1##*dir=}
         dir=${dir%/*}
-        dldir="${HOME}/Downloads/"
-        mkdir -p "$dldir"
         for i in "$@";do
             f=$(echo "${i##*/}" | unquote)
-            if ! [ -f "${dldir}/${f}" ];then
+            if ! [ -f "${DLDIR}/${f}" ];then
+                wget -nc -q -P "$DLDIR" "${domain}/$i"
                 notify-send -i "$dlicon" "Erai Sub Downloader" "$f"
-                wget -nc -q -P "$dldir" "${domain}/$i"
             fi
         done
     elif [[ "$1" =~ ^http ]];then
@@ -51,29 +52,63 @@ main() {
         dir=$(echo ${1##*dir=} | quote)
         url="${domain}/?dir=${dir}"
     fi
-    [ -n "$url" ] &&
-        curl -s "$url" | awk '/<div id="content"/,EOF {print $0}' > "$tmpfile"
+
+    if [ -n "$url" ];then
+        curl -s "$url" |
+        awk '/<div id="content"/,EOF {print $0}' > "$tmpfile"
+    fi
 
     {
-        grep -ioP '(?<=href\=")Sub/.*\.(ass|srt)(?=")' "$tmpfile";
+        grep -ioP '(?<=href\=")Sub/.*\.(ass|srt)' "$tmpfile";
         grep -oP '(?<=href\="\?dir\=)Sub[^"]*' "$tmpfile";
     } | unquote | sort
 }
+download_all() {
+    url=
+    if [[ "${1##*.}" =~ (ass|srt) ]];then
+        dir=${1##*dir=}
+        dir=${dir%/*}
+        for i in "$@";do
+            f=$(echo "${i##*/}" | unquote)
+            if ! [ -f "${DLDIR}/${f}" ];then
+                echo "$f"
+                wget -nc -q -P "$DLDIR" "${domain}/$i"
+                notify-send -i "$dlicon" "Erai Sub Downloader" "$f"
+            fi
+        done
+        return
+    else
+        dir=$(echo ${1##*dir=} | quote)
+        subdir=${dir%/*}
+        url="${domain}/?dir=${dir}/"
+    fi
+    if [ -n "$url" ];then
+        echo "$url"
+        curl -s "$url" |
+        awk '/<div id="content"/,EOF {print $0}' > "$tmpfile"
+    fi
+    {
+        grep -ioP '(?<=href\=")Sub.*\.por\.(ass|srt)' "$tmpfile" || grep -ioP '(?<=href\=")Sub.*\.(ass|srt)' "$tmpfile";
+        grep -oP '(?<=href\="\?dir\=)Sub[^"]*(?=")' "$tmpfile";
+    } | grep -vxF "$subdir" | grep -vxF "$dir" | unquote | while read -r i; do
+        download_all "$i"
+    done
+}
 favorite() {
-    if ! grep -qF "$1" "$cache" 2>/dev/null;then
+    if ! grep -qF "$1" "$favorites" 2>/dev/null;then
         notify-send -i "$favicon" "Erai Sub Downloader" "$1"
-        echo "$1" >> "$cache";
+        echo "$1" >> "$favorites";
     else
         notify-send -i "$delicon" "Erai Sub Downloader" "$1"
-        n=$(grep -nF "$1" "$cache" | cut -d':' -f1)
-        sed -i "${n}d" "$cache"
+        n=$(grep -nF "$1" "$favorites" | cut -d':' -f1)
+        sed -i "${n}d" "$favorites"
     fi
 }
-export -f main unquote quote favorite
+export -f main unquote quote favorite download_all
 case "$1" in
     -f|--favorites)
-        [ -s "$cache" ] || exit 1
-        cat "$cache"
+        [ -s "$favorites" ] || exit 1
+        cat "$favorites"
     ;;
     -r|--release)
         q=$(date +%q)
@@ -91,5 +126,6 @@ case "$1" in
 esac | fzf -m --header '^f favorite'    \
     --bind 'ctrl-t:last'                \
     --bind 'ctrl-b:first'               \
-    --bind 'enter:reload(main {+})'     \
-    --bind 'ctrl-f:execute(favorite {})'
+    --bind 'enter:reload(main {+})+clear-query'     \
+    --bind 'ctrl-f:execute(favorite {})' \
+    --bind 'ctrl-d:execute(download_all {})'
