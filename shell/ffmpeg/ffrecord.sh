35f06a8f06fbd1bedf96dcaab1f1618e2ab6c164
A1337Xyz <blindwizard@tutanota.com>
2022-08-02 21:09:56 -0300

    Update ffrecord.sh

--- shell/ffmpeg/ffrecord.sh
+++ shell/ffmpeg/ffrecord.sh
@@ -1,39 +1,38 @@
-#!/bin/sh
-# shellcheck disable=SC2046
+#!/usr/bin/env bash
+pgrep -x ffmpeg && pkill -15 -x ffmpeg
 
-case "$1" in
-    -*|help|?) printf 'Usage: %s [audio]\n' "${0##*/}" ; exit 0 ;;
-esac
+parse_display() {
+    sed 's/\([0-9]*\)x\([0-9]*\)\(.[0-9]*\)\(.[0-9]*\)/\1 \2 \3 \4/'
+}
+get_scr() {
+    regex="(\w*) connected.* ([0-9]*x[0-9]*\+[0-9]*\+[0-9]*)"
+    xrandr -q | while read -r i;do
+        if [[ "$i" =~ $regex ]];then
+            a="${BASH_REMATCH[1]}"
+            b="${BASH_REMATCH[2]}"
+            echo -n "$a "
+            echo "$b" | parse_display
+        fi
+    done
+    # xrandr -q | grep ' connected' | grep -oP '\d*x\d*\+\d*\+\d*' | tr 'x+' ' '
+}
 
-pgrep -f ffmpeg && pkill -15 ffmpeg
-icon=simplescreenrecorder 
-for i in $(seq 3 -1 1);do
-    notify-send -t 500 -i "$icon" "$i"
-    sleep 0.5
-done
-notify-send -i "$icon" "Recording now..."
+if [ "$1" = "-x" ];then
+    read w h x y < <(xwininfo | grep -oP '(?<= -geometry ).*' | parse_display)
+elif [ "$1" = "-s" ];then
+    read w h x y < <(slop | parse_display)
+else
+    read _ w h x y < <(get_scr | fzf)
+fi
 
-#alsa_input=alsa_input.pci-0000_00_0e.0.analog-stereo
-alsa_input=alsa_output.pci-0000_00_0e.0.analog-stereo.monitor
+[ -z "$w" ] && exit
+w=$(( w - ( w % 2 ) ))
+h=$(( h - ( h % 2 ) )) 
+echo ">>> ${w}x${h} $x $y"
 
-case "$1" in
-    audio) 
-        ffmpeg -hide_banner -v 33 -y -video_size 1366x768 \
-            -r 25 -f x11grab -i :0 -f pulse -ac 2 -i "$alsa_input" \
-            -c:v h264 -crf 18 -profile:v baseline -pix_fmt yuv420p \
-            ~/record_$(date +%d%m%Y%H%M%S).mp4
-    ;;
-    hdmi)
-        echo hdmi
-        ffmpeg -hide_banner -v 16 -stats -y -video_size 1280x1024 \
-            -r 25 -f x11grab -i :0 -c:v h264 \
-            -profile:v baseline -pix_fmt yuv420p \
-            ~/record_$(date +%d%m%Y%H%M%S).mp4
-    ;;
-    *)
-        ffmpeg -hide_banner -v 16 -stats -y -video_size 1366x768 \
-            -r 25 -f x11grab -i :0 -c:v h264 \
-            -profile:v baseline -pix_fmt yuv420p \
-            ~/record_$(date +%d%m%Y%H%M%S).mp4
-    ;;
-esac
+out=~/record_$(date +%Y%m%d%H%M%S).mp4
+ffmpeg -hide_banner -v 16 -stats -y -f x11grab \
+    -framerate 24 \
+    -video_size "${w}x${h}" -i ":0.0$x,$y" \
+    -f pulse -ac 2 -i default               \
+    -c:v h264 "$out"
