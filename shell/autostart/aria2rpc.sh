b225bec950636580b9f7d2df9de398c53670954b
A1337Xyz <blindwizard@tutanota.com>
2022-09-05 13:21:33 -0300

    Update aria2rpc.sh

--- shell/autostart/aria2rpc.sh
+++ shell/autostart/aria2rpc.sh
@@ -1,8 +1,11 @@
-#!/usr/bin/env bash
+#!/bin/sh
+
+set -e
 
 session=~/.cache/aria2/session
-if [ -f "$session" ];then
-    tmux new-session -d aria2c -V --enable-rpc --input-file "$session"
-else
-    tmux new-session -d aria2c -V --enable-rpc
-fi
+
+pgrep aria2c | xargs -r kill
+aria2c -D --enable-rpc --input-file "$session"
+sleep 3
+pgrep -f 'python3.*aria2bt/watch.py' | xargs -r kill
+python3 ~/.scripts/python/aria2bt/watch.py &
