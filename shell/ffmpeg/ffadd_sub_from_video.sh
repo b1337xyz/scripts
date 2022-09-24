59a1b781f97aa72e2046612ede955d8c98017c23
A1337Xyz <blindwizard@tutanota.com>
2022-09-24 15:14:04 -0300

    2022.09.24

--- shell/ffmpeg/ffadd_sub_from_video.sh
+++ shell/ffmpeg/ffadd_sub_from_video.sh
@@ -3,12 +3,13 @@
 set -e
 
 output=new_"${1##*/}"
-ffmpeg -i "$1" -i "$2" -map_metadata 0 -map 0:v -map 0:a \
-    -map 1:s:m:language:por \
-    -map 0:s:m:language:eng?        \
-    -map 0:t? -map 1:t?             \
+ffmpeg -i "$1" -i "$2" -map_metadata 0 -map 0:v \
+    -map 0:a:m:language:jpn \
+    -map 1:s \
+    -map 1:t? \
     -map -v:m:mimetype:image/jpeg?  \
     -metadata:s:s:0 language=por        \
     -metadata:s:s:0 title='Portuguese'  \
-    -disposition:s:0 default            \
+    -disposition:s:s 0 \
+    -disposition:s:0 default \
     -c copy "$output"
