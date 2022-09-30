5ac88674aebe267f13b984461b43fbb5ef25f000
A1337Xyz <blindwizard@tutanota.com>
2022-09-30 14:07:28 -0300

    2022.09.30

--- shell/4chan-dl.sh
+++ shell/4chan-dl.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
+icon=~/.local/share/icons/4chan.png
 
-url="${1%#*}"
-board=$(echo "$url" | grep -oP '(?<=\.org/).*(?=/thread)')
-thread=$(echo "$url" | grep -oP '(?<=/thread/)\d*')
-subject=$(curl -s "https://a.4cdn.org/${board}/catalog.json" |
-    jq -r '.[]["threads"][] | select(.no == '"$thread"') | .semantic_url | split("")[:60] | add')
-subject=${subject:-$thread}
-dl_dir=~/Downloads/4ch/"${board}_$(date +%Y%m%d)/${subject}"
-curl -L -s "$url" | grep -oP '(?<=href=")[^"]*\.(jpg|png|gif|webm)' | sort -u |
-    xargs -P 3 -rI{} wget -nc -nv -P "$dl_dir" "http:{}" 2>&1 | grep -v SSL_INIT
+main() {
+    url="${1%#*}"
+    board=$(echo "$url" | grep -oP '(?<=\.org/).*(?=/thread)')
+    thread=$(echo "$url" | grep -oP '(?<=/thread/)\d*')
+    subject=$(curl -s "https://a.4cdn.org/${board}/catalog.json" |
+        jq -r '.[]["threads"][] | select(.no == '"$thread"') | .semantic_url[:60]')
+    subject=${subject:-$thread}
+    dl_dir=~/Downloads/4ch/"${board}/${subject}"
+
+    notify-send -i "$icon" "thread: $thread" "$subject"
+    curl -L -s "$url" | grep -oP '(?<=href\=")[^"]*\.(jpg|png|gif|webm)' | sort -u |
+        xargs -P 3 -rI{} wget -nc -nv -P "$dl_dir" "http:{}" 2>&1 | grep -v SSL_INIT
+}
+
+[ -n "$1" ] && main "$1"
--- shell/mpc/playing_now.sh
+++ shell/mpc/playing_now.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/env bash
 
 COVER=~/.cache/thumbnails/albums
-
-main() {
-    IFS='|' read -r fpath artist album title duration < <(
-        mpc -f '%file%#|%artist%#|%album%#|%title%#|%time%' | head -1)
-    fpath=~/Music/"$fpath"
-    fname=${fpath##*/}
-    title=${title:-$fname}
-    img=$(md5sum "$fpath" | awk '{print $1".jpg"}')
-    img=${COVER}/${img}
-    [ -f "$img" ] || ffmpeg -hide_banner -v -8 -i "$fpath" "$img"
-    notify-send -i "$img" "♫ Playing now..." "$title\n$album\n$artist\n$duration"
-}
-main 2>/dev/null
+IFS='|' read -r fpath artist album title duration < <(
+    mpc -f '%file%#|%artist%#|%album%#|%title%#|%time%' | head -1)
+fpath=~/Music/"$fpath"
+fname=${fpath##*/}
+title=${title:-$fname}
+img=$(md5sum "$fpath" | awk '{print $1".jpg"}')
+img=${COVER}/${img}
+[ -f "$img" ] || ffmpeg -hide_banner -v -8 -i "$fpath" "$img"
+notify-send -i "$img" "♫ Playing now..." "$title\n$album\n$artist\n$duration"
