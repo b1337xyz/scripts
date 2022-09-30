004f5c228792e4bfe89ea69d30204330f08df37c
A1337Xyz <blindwizard@tutanota.com>
2022-09-30 14:04:38 -0300

    2022.09.30

--- shell/imagemagick/make_rice.sh
+++ shell/imagemagick/make_rice.sh
@@ -4,7 +4,7 @@ output="rice_$(date +%Y%m%d%H%M).jpg"
 declare -a arr=()
 while IFS= read -d $'\0' -r i;do
     arr+=("$i")
-done < <(nsxiv -oqt . | tr \\n \\0)
+done < <(nsxiv -oqt . 2>/dev/null | tr \\n \\0)
 [ "${#arr[@]}" -lt 2 ] && exit 1
 
 case "$1" in
