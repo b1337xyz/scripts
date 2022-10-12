b7753af78ea46ff6d086eadc3fe75126f74ff8f9
A1337Xyz <blindwizard@tutanota.com>
2022-10-12 20:00:11 -0300

    2022.10.12

--- shell/ffmpeg/ffopus.sh
+++ shell/ffmpeg/ffopus.sh
@@ -2,7 +2,6 @@
 set -e
 
 # https://github.com/slhck/ffmpeg-normalize/issues/98
-# -af "channelmap=channel_layout=5.1"
 
 out=opus_${1##*/}
 if ffmpeg -i "$1" 2>&1 | grep -q ' 5.1(side),'
@@ -14,7 +13,6 @@ then
         -c copy -c:a libopus    \
         -af "channelmap=channel_layout=5.1" "$out" 
 else
-    out=opus_${1##*/}
     ffmpeg -nostdin -v 24 -stats -i "$1"     \
         -map_metadata 0 -map 0:v  \
         -map 0:a:m:language:jpn \
