881accb567f0094e4f9fc0ce435a0364e5a93ee5
A1337Xyz <blindwizard@tutanota.com>
2022-11-26 07:27:32 -0300

    2022.11.26

--- shell/autostart/set_screen.sh
+++ shell/autostart/set_screen.sh
@@ -2,5 +2,8 @@
 if xrandr | grep -q 'HDMI1 connected';then
     xrandr --output eDP1 --primary --auto --output HDMI1 --left-of eDP1 --auto
     pgrep -x i3 && i3-msg restart 
-    sleep 1 && sh ~/.cache/xwallpaper
+    if [ -x ~/.cache/xwallpaper ];then
+        sleep 1
+        ~/.cache/xwallpaper
+    fi
 fi
