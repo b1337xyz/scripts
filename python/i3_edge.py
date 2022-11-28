4c17751dbd076336715925e7989ba10467cc492b
A1337Xyz <blindwizard@tutanota.com>
2022-11-28 19:27:28 -0300

    2022.11.28

--- python/i3_edge.py
+++ python/i3_edge.py
@@ -16,9 +16,23 @@ win_height = focused.rect.height
 x = focused.rect.x
 y = focused.rect.y
 bar = 24
+resize = '-r' in args
 
 if 'off' in focused.floating:
     focused.command('floating toggle')
+
+if resize and ('up' in args or 'down' in args):
+    x = scr_x
+    win_width  = workspace.rect.width
+    win_height = workspace.rect.height // 2
+if resize and ('left' in args or 'right' in args):
+    y = bar
+    win_width  = workspace.rect.width // 2
+    win_height = workspace.rect.height
+if resize and 'center' in args:
+    win_width  = workspace.rect.width  // 2
+    win_height = workspace.rect.height // 2
+
 if 'up' in args:
     y = bar
 if 'down' in args:
@@ -31,4 +45,6 @@ if 'center' in args:
     x = scr_x + (workspace.rect.width  // 2) - (win_width  // 2)
     y = scr_y + (workspace.rect.height // 2) - (win_height // 2)
 
+if resize:
+    con.command('resize set {} {}'.format(win_width, win_height))
 con.command('move position {} {}'.format(x, y))
