071f68f0b26653f5901943a7ff81530660836d0d
A1337Xyz <blindwizard@tutanota.com>
2022-11-29 19:14:15 -0300

    2022.11.29

--- shell/mkvtoolnix/merge_sub_from_video.sh
+++ shell/mkvtoolnix/merge_sub_from_video.sh
@@ -9,7 +9,7 @@
 # "\(.type): \(.id) - \(.properties.language) - \(.properties.track_name) \(
 # if .properties.default_track then "(default)" else "" end)"'
 
-mkvmerge -o new_"${1##*/}" -S -M -a jpn "$1" -D -A "$2"
+mkvmerge -o new_"${1##*/}" --no-chapters -S -M -a 1 "$1" -D -A "$2"
 # mkvmerge -o new_"${1##*/}" "$1" -D -A "$2"
 # mkvmerge -o new_"${1##*/}" -S -M "$1" -A -D --no-chapters --language 2:por --track-name 2:"Portuguese" "$2"
 # mkvmerge -o new_"${1##*/}" --default-track 2:0 --forced-track 2:0 "$1" -A -D --no-chapters --language 2:por --track-name 2:"Portuguese" "$2"
