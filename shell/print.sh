dee5b3e79ba52104a4a089c77668b17946bec824
A1337Xyz <blindwizard@tutanota.com>
2022-12-30 16:23:25 -0300

    2022.12.30

--- shell/print.sh
+++ shell/print.sh
@@ -1,7 +1,5 @@
-#!/bin/sh
+#!/usr/bin/env bash
 # shellcheck disable=SC2016
-set -e
-
 command -v scrot >/dev/null 2>&1 || { printf 'install scrot\n'; exit 1; }
 command -v xclip >/dev/null 2>&1 || { printf 'install xclip\n'; exit 1; }
 
@@ -14,21 +12,19 @@ export -f notify
 DIR=~/Pictures/screenshots
 [ -d "$DIR"  ] || mkdir -vp "$DIR"
 cmd='notify "$f" "$n" "$wx$h"'
-cmd2='xclip -sel clip -t image/png -i $f; notify "$f" "$n" "$wx$h"'
+copy='xclip -sel clip -t image/png -i $f; notify "$f" "$n" "$wx$h"'
 image="${DIR}/scr_%Y%m%d%H%M%S.png"
 tmpimg="/tmp/scr_%Y%m%d%H%M%S.png"
 
+sleep 1
 case "$1" in
-    -*|help)
-        printf 'Usage: %s [focused|select|copy|copy-focused|copy-select]\n' "${0##*/}"
-        exit 0
-    ;;
-    focused) scrot -q 100 -u "$image" -e "$cmd" ;;
-    select|sel) sleep 1 ; scrot -q 100 -s "$image" -e "$cmd" ;;
-    copy) scrot -q 100  "$tmpimg" -e "$cmd2" ;;
-    copy-focused|copf) scrot -q 100 -u "$tmpimg" -e "$cmd2" ;;
-    copy-select|selc) sleep 1 ; scrot -q 100 -s "$tmpimg" -e "$cmd2" ;;
-    *) scrot -q 100 -m "$image" -e "$cmd" ;;
+    -*|help) printf 'Usage: %s [focused|select|copy|copf|selc]\n' "${0##*/}"; exit 0 ;;
+    foc*) scrot -q 100 -u "$image"  -e "$cmd" ;;
+    sel*) scrot -q 100 -s "$image"  -e "$cmd" ;;
+    copf) scrot -q 100 -u "$tmpimg" -e "$copy" ;;
+    selc) scrot -q 100 -s "$tmpimg" -e "$copy" ;;
+    copy) scrot -q 100 -m "$tmpimg" -e "$copy" ;;
+    *)    scrot -q 100 -m "$image"  -e "$cmd" ;;
 esac
 
 [ -f "$tmpimg" ] && sleep 60 && rm "$tmpimg"
