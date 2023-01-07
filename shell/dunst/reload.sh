fee2db9c61eb3682d374e0f8d217174a3570a75d
A1337Xyz <blindwizard@tutanota.com>
2023-01-07 15:50:34 -0300

    2023.01.07

--- shell/dunst/reload.sh
+++ shell/dunst/reload.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-pkill -x dunst && $(dunst >/dev/null 2>&1 &)
-sleep 0.3
+pkill -x dunst #  && $(dunst >/dev/null 2>&1 &)
 notify-send -u low "Low notification" "$(uname -a)" 
 notify-send -u normal "Normal notification" "$(uname -a)" 
 notify-send -u critical "Critical notification" "$(uname -a)" 
