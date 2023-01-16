6b1607ad6a07c6ca3561296bb0e1f9aa1bcfd4e6
A1337Xyz <blindwizard@tutanota.com>
2023-01-16 09:33:08 -0300

    2023.01.16

--- shell/toggle_transparency.sh
+++ shell/toggle_transparency.sh
@@ -15,7 +15,7 @@ if pgrep -x picom >/dev/null 2>&1;then
     sed -i 's/i3bar/# i3bar/; s/\(set \$b0 #.\{6\}\).*/\1/' ~/.config/i3/theme
     sed -i 's/own_window_argb/-- own_window_argb/g' ~/.config/conky/conky.conf ~/.config/conky/conky.2.conf
 else
-    picom -b #--experimental-backends
+    picom -b # --experimental-backends
     sed -i 's/opacity: [0-9]\.[0-9]*/opacity: 0.80/' ~/.config/alacritty/alacritty.yml
     sed -i 's/# i3bar/i3bar/; s/\(set \$b0 #.\{6\}\).*/\18f/' ~/.config/i3/theme
     sed -i 's/-- own_window_argb/own_window_argb/g' ~/.config/conky/conky.conf ~/.config/conky/conky.2.conf
