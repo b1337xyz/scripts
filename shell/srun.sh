c90db4e7f5e3a9cc501e360b0e98ad58ba0f51d3
A1337Xyz <blindwizard@tutanota.com>
2023-02-06 15:38:55 -0300

    add game startup notification

--- shell/srun.sh
+++ shell/srun.sh
@@ -2,11 +2,12 @@
 set -e
 
 cd ~/.local/share/applications
-grep -rn 'Exec=steam' | cut -d':' -f1 | while read -r i;do
-    grep -oP '(?<=Name=).*' "$i"
-done | dmenu -c -l 10 | while read -r i;do
-    file=$(grep -rnF "Name=$i" | cut -d':' -f1)
+grep -rn 'Exec=steam' | while read -r i; do
+    grep -oP '(?<=Name=).*' "${i%%:*}"
+done | dmenu -c -l 10 | while read -r name; do
+    file=$(grep -rnF "Name=$name" | cut -d':' -f1)
     if [ -f "$file" ];then
+        notify-send "Executing..." "$name"
         exec=$(grep -oP '(?<=Exec=).*' "$file")
         i3-msg "exec --no-startup-id $exec"
     fi
