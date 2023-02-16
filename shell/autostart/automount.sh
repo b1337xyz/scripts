93b1b45d7b625508a828aa82e0c8c67b7ddfb640
A1337Xyz <blindwizard@tutanota.com>
2023-02-16 20:10:22 -0300

    2023.02.16

--- shell/autostart/automount.sh
+++ shell/autostart/automount.sh
@@ -14,22 +14,26 @@ trap 'rm "$lock" 2>/dev/null' EXIT INT HUP
 get_devname() {
     udevadm info -p /sys/"$1" | awk -v FS== '/DEVNAME/{print $2}'
 }
+get_label() {
+    lsblk -o LABEL,UUID "$1" | tail -1
+}
 
-stdbuf -oL -- udevadm monitor --udev -s block | while read -r -- _ _ event devpath _
-do
+stdbuf -oL -- udevadm monitor --udev -s block | while read -r -- _ _ event devpath _; do
     if [ "$event" = add ]; then
         devname=$(get_devname "$devpath")
         [[ "$devname" =~ [0-9]$ ]] || continue
         grep -q "^$devname" /proc/mounts && continue
-        read -r label uuid < <(lsblk -o LABEL,UUID "$devname" | tail -1)
+
+        read -r label uuid < <(get_label "$devname")
         label=${label:-$uuid}
         [ -z "$label" ] && continue
+
         mp="${MP}/$label"
         [ -d "$mp" ] || mkdir -vp "$mp"
         sudo mount "$devname" "$mp" -o noatime
         ln -vsf "$mp" ~/mnt
+
         notify-send -i drive-harddisk "$label mounted" "$mp" 2>/dev/null || true
         # udisksctl mount --no-user-interaction -b "$devname" -o noatime
-        sleep 1
     fi
 done
