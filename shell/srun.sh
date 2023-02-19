b4151d40bbc8abe0f614f5d8edeb35a3c5f6ef86
A1337Xyz <blindwizard@tutanota.com>
2023-02-19 08:12:44 -0300

    2023.02.19

--- shell/srun.sh
+++ shell/srun.sh
@@ -4,8 +4,8 @@ set -e
 cd ~/.local/share/applications
 grep -rn 'Exec=steam' | while read -r i; do
     grep -oP '(?<=Name=).*' "${i%%:*}"
-done | dmenu -i -c -l 10 | while read -r name; do
-    file=$(grep -rnF "Name=$name" | cut -d':' -f1)
+done | sort -V | dmenu -i -c -l 10 | while read -r name; do
+    file=$(grep -rnxF "Name=$name" | cut -d':' -f1)
     if [ -f "$file" ];then
         notify-send "Executing..." "$name"
         exec=$(grep -oP '(?<=Exec=).*' "$file")
