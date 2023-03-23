98ba4c620270a15c1cb7956dbfde3b62657ef3c0
A1337Xyz <blindwizard@tutanota.com>
2023-03-22 22:14:27 -0300

    2023.03.22

--- shell/nsxiv/sxmpv.sh
+++ shell/nsxiv/sxmpv.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 ptr='.*\.(mp4|mkv|webm|avi)'
-tmpdir=/tmp/sxiv
-mkdir -p "$tmpdir"
+THUMB=~/.cache/thumbnails/video
 find . -maxdepth 1 -regextype posix-extended \
     -iregex "$ptr" -type f | sort -V | while read -r i
 do
-    img="${tmpdir}/${i##*/}.jpg"
-    test -f "$img" || ffmpegthumbnailer -s 300 -i "$i" -o "$img" 2>/dev/null
+    img="${THUMB}/${i##*/}.jpg"
+    if ! [ -f "$img" ];then
+        mkdir -p "${img%/*}"
+        ffmpegthumbnailer -s 0 -i "$i" -o "$img" 2>/dev/null
+    fi
     printf '%s\n' "$img"
 done | sxiv -qito | sed 's/.*\//\.\//; s/\.jpg$//'
