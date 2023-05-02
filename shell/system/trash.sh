19705d925cc2bbd8d6607cbe52cc762f9bc46554
A1337Xyz <blindwizard@tutanota.com>
2023-05-02 20:52:59 -0300

    2023.05.02

--- shell/system/trash.sh
+++ shell/system/trash.sh
@@ -2,36 +2,20 @@
 set -e
 
 INFO_FILE=~/.local/share/mytrash/info
-TRASH_DIR=~/.local/share/mytrash/files
+TRASH_DIR=~/.local/share/mytrash
 [ -d "$TRASH_DIR" ] || mkdir -p "$TRASH_DIR"
 
-help() {
-    printf 'Usage: %s -r <files*>\n' "${0##*/}"; exit 1
-}
-[ "$1" ] || help
-
-preview() {
-    stat -c '%x' "$1"
-    file -bi -- "$1" | grep -q '^text' && bat --color=always --style=plain -- "$1"
-}
-export -f preview
-
-case "$1" in
-    -r)
-        while read -r i;do
-            mv -vi -- "$i" . && rm -d "${i%/*}"
-        done < <(find "$TRASH_DIR" -mindepth 2 -maxdepth 2 -printf '%T@ %p\n' | sort -rn | cut -d' ' -f2- |
-                 fzf -m -d '/' --no-sort --with-nth -1 --preview-window 'right:60%' --preview 'preview {}')
-    ;;
-    -*) help ;;
-    *)
-        for i in "$@";do
-            if [ -e "$i" ];then
-                rp=$(realpath "$i")
-                destdir=$(mktemp -d "${TRASH_DIR}/trash-XXXXXXXXXX")
-                mv -v -- "$rp" "$destdir"
-                echo "[$(date '+%Y-%m-%d %H:%M')] '$rp' -> '$destdir'" >>"$INFO_FILE"
-            fi
-        done
-    ;;
-esac
+if [ -e "$1" ];then
+    for i in "$@";do
+        destdir=$(mktemp -d "${TRASH_DIR}/trash-XXXXXXXXXX")
+        mv -v -- "$i" "$destdir" || { rm -d "$destdir"; exit 1; }
+        rp=$(realpath -- "$i")
+        echo "[$(date '+%Y-%m-%d %H:%M')] '$rp' -> '$destdir'" >>"$INFO_FILE"
+    done
+else
+    while read -r i;do
+        mv -vi -- "$i" . && rm -d "${i%/*}"
+    done < <(find "$TRASH_DIR" -mindepth 2 -maxdepth 2 -printf '%T@ %p\n' |
+             sort -rn | cut -d' ' -f2- |
+             fzf -m -d '/' --no-sort --with-nth -1)
+fi
