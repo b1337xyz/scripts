93a855f7542798797e18fd62e8e64daf7a39f102
A1337Xyz <blindwizard@tutanota.com>
2023-06-08 00:15:41 -0300

    2023.06.08

--- python/ehentai.py
+++ python/ehentai.py
@@ -3,28 +3,22 @@ from html import unescape
 from random import random
 from sys import argv
 from time import sleep
-import logging
+from http.cookiejar import MozillaCookieJar
+from pathlib import Path
 import os
 import re
 import requests
-import subprocess as sp
+
+requests.packages.urllib3.disable_warnings()
 
 UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:102.0) Gecko/20100101 Firefox/102.0'  # noqa: E501
 HOME = os.getenv('HOME')
 CACHE_DIR = os.getenv('XDG_CACHE_HOME', os.path.join(HOME, '.cache'))
 LOG = os.path.join(CACHE_DIR, 'ehentai.log')
 DL_DIR = os.path.join(HOME, 'Downloads/e_hentai')
+COOKIE_FILE = os.path.join(CACHE_DIR, 'ehentai.cookie')
+HIST = os.path.join(CACHE_DIR, 'ehentai.history')
 MAX_ATTEMPS = 5
-
-
-logging.basicConfig(
-    filename=LOG,
-    encoding='utf-8',
-    filemode='a',
-    level=logging.INFO,
-    format='%(asctime)s:%(levelname)s: %(message)s',
-    datefmt='%d-%m-%Y %H:%M:%S',
-)
 
 
 gallery_regex = re.compile(r'href=\"https://e-hentai\.org/g/(\d*/[^/]*)/')
@@ -45,22 +39,31 @@ def clean_filename(s: str) -> str:
     return re.sub(r'\s{2,}', ' ', s).strip()
 
 
-def download(url: str, dl_dir: str) -> int:
-    p = sp.run(['aria2c', url, '-q', '--dir', dl_dir, '-U', UA,
-                '--always-resume=false',
-                '--max-resume-failure-tries=0',
-                '--force-save=false',
-                '--auto-file-renaming=false'])
-    return p.returncode
+def mkdir(path):
+    Path(path).mkdir(exist_ok=True, parents=True)
 
 
-def random_sleep():
-    sleep(random() * .55)
+def get(s, url, stream=False):
+    sleep(random() * .99)
+    return s.get(url, stream=stream, verify=False)
+
+
+def download(s, url, filepath):
+    if os.path.exists(filepath):
+        print(filepath)
+        return
+
+    tempfile = f'{filepath}.temp'
+    r = get(s, url, stream=True)
+    with open(tempfile, 'wb') as f:
+        f.write(r.content)
+
+    os.rename(tempfile, filepath)
+    print(f'\033[1;32m{filepath}\033[m')
 
 
 def get_galleries(s, url):
-    s.headers.update({'user-agent': UA})
-    r = s.get(url)
+    r = get(s, url)
     curr_page = page_regex.search(url)
     curr_page = 1 if not curr_page else int(curr_page.group(1))
     try:
@@ -70,25 +73,30 @@ def get_galleries(s, url):
 
     galleries = list()
     for page in range(curr_page, max_page + 1):
-        random_sleep()
         for link in gallery_regex.findall(r.text):
             gid, token = link.split('/')
             galleries.append([gid, token])
+
         if max_page > curr_page:
             if page_regex.search(url):
                 url = re.sub(r'([\?\&]page)=(\d+)', r'\1={}'.format(page), url)
             else:
                 url += f'&page={page}' if '?' in url else f'?page={page}'
-            r = s.get(url)
+            r = get(s, url)
     return galleries
 
 
 def main(url):
     assert 'e-hentai.org' in url
-    sp.run(['notify-send', 'E-hentai downloader started', url])
-    logging.info(f'{url = }')
+    open(HIST, 'a').write(f'{url}\n')
 
     s = requests.Session()
+    s.headers.update({'user-agent': UA})
+    if os.path.exists(COOKIE_FILE):
+        cj = MozillaCookieJar(COOKIE_FILE)
+        cj.load(ignore_discard=True, ignore_expires=True)
+        s.cookies = cj
+
     if re.search(r'\.org/g/\d+', url):
         gid, token = re.search(r'/g/(\d+)/([^/]*)', url).group(1, 2)
         galleries = [(gid, token)]
@@ -98,20 +106,19 @@ def main(url):
     c = 0
     total = len(galleries)
     for gid, token in galleries:
-        random_sleep()
         c += 1
         url = f'https://e-hentai.org/g/{gid}/{token}/'
         try:
-            r = s.get(url)
+            r = get(s, url)
         except Exception as err:
-            logging.error(f'error requesting: {url}, {r.status_code}, {err}')
+            print(f'request failed, {url}, {r.status_code}, {err}')
             continue
 
         try:
             url = re.search(r'https://e-hentai\.org/s/[^/]*/\d*-1', r.text)
             url = url.group()
         except AttributeError:
-            logging.error(f'nothing found: {url}')
+            print(f'nothing found, {url}')
             continue
 
         try:
@@ -126,7 +133,7 @@ def main(url):
             except AttributeError:
                 title = gid
         title = clean_filename(unescape(title))
-        logging.info(f'gallery {c} of {total}: {title} {url}')
+        print(f'gallery {c} of {total}: {title} {url}')
 
         try:
             artist = artist_regex.search(r.text).group(1)
@@ -135,37 +142,39 @@ def main(url):
             dl_dir = os.path.join(DL_DIR, title)
 
         if skip_regex and skip_regex.search(title):
-            logging.info(f'skipping: {title}')
+            print(f'skipping: {title}')
             continue
+
+        mkdir(dl_dir)
 
         curr_page = 0
         next_page = curr_page + 1
-        att = 0
         while next_page > curr_page:
-            att += 1
-            if att > MAX_ATTEMPS:
-                break
-
-            r = s.get(url)
-            random_sleep()
-
-            try:
-                img = img_regex.search(r.text).group(1)
-            except AttributeError:
-                logging.error(f'image not found, {url}, attempt: {att} of {MAX_ATTEMPS}')  # noqa: E501
-                continue
-
-            if '/509.gif' in img:
-                logging.warning(f'509 ERROR, {url} - {img}')
-                break
-
-            exit_code = download(img, dl_dir)
-            # 13 - If file already existed
-            if exit_code not in [0, 13] and att < MAX_ATTEMPS:
-                logging.error(f'aria2c error: {exit_code}, {url}, attempt: {att} of {MAX_ATTEMPS}')  # noqa: E501
-                continue
-
             att = 0
+            while att < MAX_ATTEMPS:
+                att += 1
+                r = get(s, url)
+                try:
+                    img = img_regex.search(r.text).group(1)
+                except AttributeError:
+                    print(f'image not found, {url}, attempt: {att} of {MAX_ATTEMPS}')  # noqa: E501
+                    continue
+
+                if '/509.gif' in img:
+                    print(f'509 IMAGE, {url} - {img}')
+                    exit(1)
+
+                filename = img.split('/')[-1].split('?')[0]
+                filename, ext = os.path.splitext(filename)
+                filename = '{}_page-{}{}'.format(filename, curr_page + 1, ext)
+                filepath = os.path.join(dl_dir, filename)
+                try:
+                    download(s, img, filepath)
+                    break
+                except Exception as err:
+                    print(err)
+                    print(f'download failed, {url}, attempt: {att} of {MAX_ATTEMPS}')  # noqa: E501
+
             curr_page = int(url.split('-')[-1])
             url = next_regex.search(r.text).group(1)
             next_page = int(url.split('-')[-1])
@@ -174,5 +183,7 @@ def main(url):
 if __name__ == '__main__':
     try:
         main(argv[1])
+    except KeyboardInterrupt:
+        pass
     except Exception as err:
-        logging.error(f'finished with errors, {err}')
+        print(f'finished with errors, {err}')
