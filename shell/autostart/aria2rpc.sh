c21f7716ea4e7added5ba95cb532784067ab1619
A1337Xyz <blindwizard@tutanota.com>
2023-06-30 13:28:11 -0300

    2023.06.30

--- shell/autostart/aria2rpc.sh
+++ shell/autostart/aria2rpc.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-pkill -15 -e -f -- 'aria2c -D -V --enable-rpc' && sleep 1
+pkill -15 -e -f -- 'aria2c -D --enable-rpc' && sleep 1
 
 session=${HOME}/.cache/aria2/session
 script=${HOME}/.local/bin/a2notify.py
-set -- -D -V --enable-rpc --continue \
+set -- -D --enable-rpc --continue \
     --on-bt-download-complete="${script}" \
     --on-download-complete="${script}" \
     --on-download-error="${script}" \
