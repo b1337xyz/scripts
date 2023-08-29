8c240e03246a104707dd0d20e6fa6906cf28625b
b1337xyz <blindwizard@tutanota.com>
2023-08-29 20:50:31 -0300

    2023.08.29

--- shell/system/print.sh
+++ shell/system/print.sh
@@ -1,33 +1,26 @@
 #!/usr/bin/env bash
-# shellcheck disable=SC2016,SC2317
-command -v scrot >/dev/null 2>&1 || { printf 'install scrot\n'; exit 1; }
-command -v xclip >/dev/null 2>&1 || { printf 'install xclip\n'; exit 1; }
-
-notify() {
-    size=$(du -sh "$1" | awk '{print $1}')
-    notify-send -i "$1" "$2" "$3\n$size"
-}
-export -f notify
+hash scrot || { printf 'install scrot\n'; exit 1; }
+hash xclip || { printf 'install xclip\n'; exit 1; }
 
 DIR=~/Pictures/screenshots
 [ -d "$DIR"  ] || mkdir -vp "$DIR"
-cmd='notify "$f" "$n" "$wx$h"'
-copy='xclip -sel clip -t image/png -i $f; notify "$f" "$n" "$wx$h"'
+cmd='notify-send -i "$f" "$n" "$wx$h\n$s"'
+copy='xclip -sel clip -t image/png -i $f; notify-send -i "$f" "$n" "$wx$h\n$s"'
 image="${DIR}/scr_%Y.%m.%d_%H%M%S.png"
 tmpimg=/tmp/scr_$(date +%Y%m%d%H%M%S).png
 
 sleep .2
 case "$1" in
     -*|help) printf 'Usage: %s [focused|select|copy|copf|selc]\n' "${0##*/}"; exit 0 ;;
-    foc*) scrot -q 100 -u "$image"  -e "$cmd" ;;
-    sel*) scrot -q 100 -s "$image"  -e "$cmd" ;;
-    copf) scrot -q 100 -u "$tmpimg" -e "$copy" ;;
-    cops) scrot -q 100 -s "$tmpimg" -e "$copy" ;;
-    copy) scrot -q 100 -m "$tmpimg" -e "$copy" ;;
-    [0-9]) sleep "$1"; bash "$0" ;;
-    *)    scrot -q 100 -m "$image"  -e "$cmd" ;;
+    foc*) scrot -u "$image"  -e "$cmd" ;;
+    sel*) scrot -f -s "$image"  -e "$cmd" ;;
+    copf) scrot -u "$tmpimg" -e "$copy" ;;
+    cops) scrot -f -s "$tmpimg" -e "$copy" ;;
+    copy) scrot -m "$tmpimg" -e "$copy" ;;
+    [0-9]*) sleep "$1"; bash "$0" ;;
+    *) scrot -m "$image"  -e "$cmd" ;;
 esac
 
-[ -f "$tmpimg" ] && sleep 60 && rm "$tmpimg"
+[ -f "$tmpimg" ] && sleep 30 && rm "$tmpimg"
 
 exit 0
