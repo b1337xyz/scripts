09795d1930ebd26bacfe4bbcf2375a0876426feb
Blind Wizard <blindwizard@tutanota.com>
2024-01-23 00:33:49 -0300

    2024.01.23

--- shell/mpv/radio.sh
+++ shell/mpv/radio.sh
@@ -14,12 +14,7 @@ set -eo pipefail
 SOCK=/tmp/mpvradio
 src=$(realpath "$0")
 conf=${src%/*}/radio.txt
-url=$(
-    cut -d'|' -f1 "$conf"   |
-    dmenu -l 20 -i -c       |
-    xargs -rI{} grep -F "{}" "$conf" |
-    cut -d'|' -f2
-)
+url=$(cut -d'|' -f1 "$conf" | dmenu -l 20 -i -c | grep -Ff - "$conf" | cut -d'|' -f2)
 if [ -S "$SOCK" ] && [ -n "$url" ]
 then
     printf '{"command": ["loadfile", "%s"]}\n' "$url" | socat - "$SOCK"
