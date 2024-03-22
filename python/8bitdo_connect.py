02fdadff08dc83569b729704197301a0c74ec3d9
Blind Wizard <blindwizard@tutanota.com>
2024-03-22 00:11:52 -0300

    2024.03.22

--- python/8bitdo_connect.py
+++ python/8bitdo_connect.py
@@ -16,12 +16,10 @@ import os
 
 for dev in hid.enumerate():
     s = dev.get('manufacturer_string')
-    print(dev)
     if s == '8BitDo':
         vendor_id, product_id = dev['vendor_id'], dev['product_id']
         gamepad = hid.device()
         gamepad.open(vendor_id, product_id)
-        print(s, 'connected')
         break
 else:
     exit(0)
