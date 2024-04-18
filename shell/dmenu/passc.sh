273397481f8d0db7e5f670e4ef96460a23de771a
Blind Wizard <blindwizard@tutanota.com>
2024-04-18 19:42:04 -0300

    2024.04.18

--- shell/dmenu/passc.sh
+++ shell/dmenu/passc.sh
@@ -3,10 +3,8 @@ set -eo pipefail
 
 if [ -n "$DISPLAY" ];then
     # passmenu -l 15 -c
-    notify-send "Password copied to clipboard"
     find "$PASSWORD_STORE_DIR" -name '*.gpg' -printf '%P\n' | 
         sed 's/\.gpg$//g' | rofi -dmenu -l 15 | xargs -r pass show -c
-
 else
     find "$PASSWORD_STORE_DIR" -name '*.gpg' -printf '%P\n' |
         sed 's/\.gpg$//g' | fzf --height 15 | xargs -ro pass ls
