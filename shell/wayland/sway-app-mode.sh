5d7782dcaf1b70d8e6c2dcbfd4acd277a04dbd40
Blind Wizard <blindwizard@tutanota.com>
2024-04-19 23:45:48 -0300

    2024.04.19

--- shell/wayland/sway-app-mode.sh
+++ shell/wayland/sway-app-mode.sh
@@ -4,21 +4,15 @@
 #         bindsym $mod+0 mode default
 # }
 
-app_mode() {
-    swaymsg -t subscribe -m '[ "window" ]' | while read -r _
-    do
-        swaymsg -r -t get_tree | jq '.. | select(.type?) | select(.focused==true).name'
-    done |
-        while read -r line
-        do
-            case $line in
-                *TigerVNC*)
-                    swaymsg mode passthrough ;;
-                *)
-                    swaymsg mode default ;;
-            esac
-            # echo "$line"
-        done
-}
-
-app_mode
+swaymsg -t subscribe -m '[ "window" ]' | while read -r _;do
+    swaymsg -r -t get_tree | jq '.. | select(.type?) | select(.focused==true).name'
+done |
+    while read -r line; do
+        case "$line" in
+            *TigerVNC*)
+                swaymsg mode passthrough ;;
+            *)
+                swaymsg mode default ;;
+        esac
+        # echo "$line"
+    done
