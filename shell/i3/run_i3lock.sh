425fa44e1a4c6d661c374264012ab03fe9b169d0
Blind Wizard <blindwizard@tutanota.com>
2024-08-14 21:23:21 -0300

    2024.08.14

--- shell/i3/run_i3lock.sh
+++ shell/i3/run_i3lock.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 res=$(xrandr -q |grep -P ' \d+x\d+[\t ]+\d+\.\d+\*\+' | awk '{print $1}' | head -1)
 # wallpaper=$(grep -oP '(?<=").*(?="$)' ~/.cache/xwallpaper)
-scrot -o -m /tmp/scrlock.png 
-convert /tmp/scrlock.png -resize "${res}!"  -blur 0x3 RGB:- |
-    i3lock -u -e --raw 1366x768:rgb --image /dev/stdin
+scrot -o -m /tmp/scrlock.png
+convert /tmp/scrlock.png -resize "${res}!" -blur 0x3 RGB:- |
+    i3lock -u -e --raw "${res}":rgb --image /dev/stdin
 
 [ "$1" = "off" ] && xset dpms force off
 
